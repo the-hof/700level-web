df2bf12fc11c71245ae31ce7d902a4a297171bd3
John Hofmann <johnhofmann@Section-9.local>
2013-12-03 11:41:44 -0500

    added Admin role to users

--- test/usertests.js
+++ test/usertests.js
@@ -6,37 +6,29 @@ describe('User', function () {
   describe('#deleteUserName', function () {
     it('should delete without error', function (done) {
       var user = new UserService();
-      user.deleteUserName('test_username', function (err) {
-        if (err) throw err;
-        done();
-      })
+      user.deleteUserName('test_username', done);
     });
 
     it('should handle the case of deleting data already deleted without error', function (done) {
       var user = new UserService();
-      user.deleteUserName('test_username', function (err) {
-        if (err) throw err;
-        done();
-      })
+      user.deleteUserName('test_username', done);
     })
   });
 
   describe('#save()', function () {
     it('should save without error', function (done) {
       var user = new UserService();
-      user.save('test_username', 'test_password', 'test.email@700level.com', function (err) {
-        if (err) throw err;
-        done();
-      });
+      user.createNew('test_username', 'test_password', 'test.email@700level.com', done);
     })
   });
 
   describe('getByUsernamePassword', function () {
     it('should find the user just saved', function (done) {
       var user = new UserService();
-      user.getByUsernamePassword('test_username', 'test_password', function (err, User) {
+      user.getByUsernamePassword('test_username', 'test_password', function (err, SelectedUser) {
         if (err) throw err;
-        expect(User.isValid).to.equal(true);
+        expect(SelectedUser.isValid).to.equal(true);
+        expect(SelectedUser.isAdmin).to.equal(false);
         done();
       })
     });
@@ -50,4 +42,47 @@ describe('User', function () {
       })
     });
   });
-})
+
+  describe('getById', function () {
+    it('should find a user previously saved', function (done) {
+      var user = new UserService();
+      user.getByUsernamePassword('test_username', 'test_password', function (err, SelectedUser) {
+        if (err) throw err;
+        user.getById(SelectedUser.id, function (err, ConfirmedUser) {
+          if (err) throw err;
+          expect(ConfirmedUser.isValid).to.equal(true);
+          expect(ConfirmedUser.isAdmin).to.equal(false);
+          done();
+        })
+      })
+    })
+
+    it('should not find randomly generated userid in database', function (done) {
+      var user = new UserService();
+      user.getById(uuid.v4(), function (err, SelectedUser) {
+        if (err) throw err;
+        expect(SelectedUser.isValid).to.equal(false);
+        done();
+      })
+    })
+  });
+
+  describe('makeAdmin', function () {
+    it('should set a user to be an admin without error', function (done) {
+      var user = new UserService();
+      user.getByUsernamePassword('test_username', 'test_password', function (err, SelectedUser) {
+        if (err) throw err;
+        user.setAdmin(SelectedUser.id, done);
+      })
+    });
+
+    it('should correctly identify the recently promoted user as an admin', function (done) {
+      var user = new UserService();
+      user.getByUsernamePassword('test_username', 'test_password', function (err, SelectedUser) {
+        if (err) throw err;
+        expect(SelectedUser.isAdmin).to.equal(true);
+        done();
+      });
+    });
+  })
+});
