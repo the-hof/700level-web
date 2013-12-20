13f465b643acc8fb80d4d1e93c92d1d6e1611889
the-hof <genghisuber@gmail.com>
2013-12-20 10:21:29 -0500

    added listPostsByThreadId method to ForumService

--- test/forumtests.js
+++ test/forumtests.js
@@ -44,7 +44,7 @@ describe('Forum', function () {
           post = 'text sample 5';
           break;
       }
-      forumService.savePost('forumtest_user', 'forumtest_userpwd', forum, thread, post, ip, function (err) {
+      forumService.savePost('forumtest_user', 'forumtest_userpwd', forum, thread, null, post, ip, function (err) {
         if (err) console.log(err);
         addRecords(err, recordsToAdd - 1, callback);
       });
@@ -119,7 +119,7 @@ describe('Forum', function () {
       var post = 'test post';
       var ip = '127.0.0.1';
 
-      forumService.savePost('forumtest_user', 'forumtest_userpwd', forum, thread, post, ip, done);
+      forumService.savePost('forumtest_user', 'forumtest_userpwd', forum, thread, null, post, ip, done);
     });
 
     it('should not allow saves by invalid username & password combos', function (done) {
@@ -129,7 +129,7 @@ describe('Forum', function () {
       var post = 'should never see this';
       var ip = '127.0.0.1';
 
-      forumService.savePost(uuid.v4(), uuid.v4(), forum, thread, post, ip, function (err) {
+      forumService.savePost(uuid.v4(), uuid.v4(), forum, thread, null, post, ip, function (err) {
         if (err && (err != 'username and password not authorized to post')) throw err;
         done();
       })
@@ -178,6 +178,79 @@ describe('Forum', function () {
       })
     })
   });
+
+  describe('#listPostsByThreadId', function () {
+    it('should error when thread id not supplied', function (done) {
+      var forumService = new ForumService();
+      var forum = 'The Barrel';
+      var thread = 'Auto test thread';
+      var threadId = '';
+      forumService.listPostsByThread(forum, thread, 1, 1, function (err, ControlPostList) {
+        if (err) throw err;
+        threadId = ControlPostList[0].thread_id;
+        forumService.listPostsByThreadId('', threadId, 1, 25, function (err, PostList) {
+          if (err && (err != 'Forum or thread not specified')) throw err;
+          expect(PostList).to.not.be.ok();
+          done();
+        })
+      })
+
+    })
+    it('should error when thread name blank', function (done) {
+      var forumService = new ForumService();
+      var forum = 'The Barrel';
+      var thread = 'Auto test thread';
+      var threadId = '';
+      forumService.listPostsByThread(forum, thread, 1, 1, function (err, ControlPostList) {
+        if (err) throw err;
+        threadId = ControlPostList[0].thread_id;
+        forumService.listPostsByThreadId(forum, '', 1, 25, function (err, PostList) {
+          if (err && (err != 'Forum or thread not specified')) throw err;
+          expect(PostList).to.not.be.ok();
+          done();
+        })
+      })
+    })
+    it('should return an array of posts', function (done) {
+      var forumService = new ForumService();
+      var forum = 'The Barrel';
+      var thread = 'Auto test thread';
+      var threadId = '';
+      forumService.listPostsByThread(forum, thread, 1, 1, function (err, ControlPostList) {
+        if (err) throw err;
+        threadId = ControlPostList[0].thread_id;
+        forumService.listPostsByThreadId(forum, threadId, 1, 25, function (err, PostList) {
+          if (err) throw err;
+          expect(PostList).to.be.an('array');
+          expect(PostList).to.not.be.empty();
+          done();
+        })
+      })
+    })
+    it('should return a sorted array of posts', function (done) {
+      var forumService = new ForumService();
+      var forum = 'The Barrel';
+      var thread = 'Auto test thread';
+      var threadId = '';
+      forumService.listPostsByThread(forum, thread, 1, 1, function (err, ControlPostList) {
+        if (err) throw err;
+        threadId = ControlPostList[0].thread_id;
+        forumService.listPostsByThreadId(forum, threadId, 1, 25, function (err, PostList) {
+          if (err) throw err;
+          expect(PostList).to.be.an('array');
+          expect(PostList).to.not.be.empty();
+          //loop through result set
+          //skipping the first one, so starting at i=1
+          for (var i = 1; i < PostList.length; i++) {
+            var prior_post_time = PostList[i - 1].last_modified;
+            var this_post_time = PostList[i].last_modified;
+            expect(this_post_time).to.be.greaterThan(prior_post_time);
+          }
+          done();
+        })
+      })
+    })
+  })
 
   describe('#listPostsByThread', function () {
     it('should error when forum name blank', function (done) {
