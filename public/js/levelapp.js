3c7ee1649e2717d51d0d180956fb6300385f3594
the-hof <genghisuber@gmail.com>
2013-12-22 18:45:58 -0500

    added forum list page and thread detail page to UI

--- public/js/levelapp.js
+++ public/js/levelapp.js
@@ -5,12 +5,20 @@ var levelApp = angular.module('levelApp', [
 
 levelApp.config(['$routeProvider',
   function($routeProvider) {
-    $routeProvider.
-      when('/forum/:forumName', {
+    $routeProvider
+      .when('/forum/:forumName/:threadId', {
+        templateUrl: 'partials/thread-detail.html',
+        controller: 'threadDetailCtrl'
+      })
+      .when('/forum/:forumName', {
         templateUrl: 'partials/forum-detail.html',
         controller: 'forumDetailCtrl'
-      }).
-      when('/', {
+      })
+      .when('/forum', {
+        templateUrl: 'partials/forum-list.html',
+        controller: 'homeCtrl'
+      })
+      .when('/', {
         templateUrl: 'partials/home.html',
         controller: 'homeCtrl'
       })
--- public/js/levelcontrollers.js
+++ public/js/levelcontrollers.js
@@ -60,3 +60,55 @@ levelControllers.controller('forumDetailCtrl', ['$scope', '$routeParams', '$http
 
   }]);
 
+levelControllers.controller('threadDetailCtrl', ['$scope','$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $scope.pageSize = 25;
+    $scope.pageNum = 1;
+    $scope.resultCount = 0;
+
+    $scope.threadId = $routeParams.threadId;
+    $scope.forumCode = $routeParams.forumName;
+    $scope.forumName = TranslateForumName($routeParams.forumName);
+
+    getThreadPage($scope.forumCode, $scope.threadId, $scope.pageSize, $scope.pageNum, function(data) {
+      $scope.postList = data;
+      $scope.resultCount = data.length;
+    })
+
+    $scope.nextPage = function() {
+      $scope.pageNum++;
+      getThreadPage($scope.forumCode, $scope.threadId, $scope.pageSize, $scope.pageNum, function(data) {
+        $scope.postList = data;
+        $scope.resultCount = data.length;
+      })
+    }
+
+    $scope.prevPage = function() {
+      $scope.pageNum--;
+      getThreadPage($scope.forumCode, $scope.threadId, $scope.pageSize, $scope.pageNum, function(data) {
+        $scope.postList = data;
+        $scope.resultCount = data.length;
+      })
+    }
+
+    function getThreadPage(forum, threadId, pageSize, pageNum, callback) {
+      var api_url = 'http://beta.700level.com/v1/forum/thread?callback=JSON_CALLBACK';
+      api_url += '&forum=' + forum;
+      api_url += '&threadId=' + threadId;
+      api_url += '&pageSize=' + pageSize;
+      api_url += '&startPage=' + pageNum;
+
+      $http.jsonp(api_url).success(callback);
+    }
+
+    function TranslateForumName(short_name) {
+      var ret = '';
+      switch (short_name) {
+        case 'nosebleeds': ret = 'Nosebleeds'; break;
+        case 'concourse': ret = 'Concourse'; break;
+        case 'parkinglot': ret = 'Parking Lot'; break;
+      }
+      return ret;
+    }
+  }]);
+
